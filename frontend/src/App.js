6b59cf24316e97e9a4870754823532fb125fc59d
Septian Wijaya <septianwijaya@192.168.1.12>
2021-08-25 21:27:14 +0700

    Starting React JS

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+const App = () => {
+
+  const hello = 1;
+
+  return (
+    <div className="App">
+      <header className="App-Header">Hello Dunia { hello }</header>
+    </div>
+  );
+}
+
+export default App;
