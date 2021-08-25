57686747afe2c9bcd15b93e6f988c20ccf2d18e9
Septian Wijaya <septianwijaya@192.168.1.12>
2021-08-25 19:18:54 +0700

    create server & install npm

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+const notes = require('./data/note');
+
+const app = express();
+dotenv.config();
+
+const PORT = process.env.PORT || 3030;
+
+app.get('/', (req, res) => {
+    res.send("API IS RUNNING")
+});
+
+app.get('/api/notes', (req, res) => {
+    res.json(notes)
+})
+
+app.get('/api/notes/:id', (req, res) => {
+    const data = notes.find((n) => n._id === req.params.id);
+    res.json(data);
+})
+
+app.listen(PORT, () => {
+    console.log(`Server Is Running in http://localhost:${PORT} !`);
+})
